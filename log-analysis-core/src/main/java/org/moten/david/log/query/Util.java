b2b034db861687a8fb19d0050f95cefcda9c8203
Dave Moten <davidmoten@gmail.com>
2012-12-16 19:00:37 +1100

    show mean and sd on graph

--- log-analysis-core/src/main/java/org/moten/david/log/query/Util.java
+++ log-analysis-core/src/main/java/org/moten/david/log/query/Util.java
@@ -1,5 +1,7 @@
 package org.moten.david.log.query;
 
+import com.google.common.base.CaseFormat;
+
 public class Util {
 
 	public static String toJson(Buckets buckets, Metric metric) {
@@ -9,16 +11,39 @@ public class Util {
 			if (value != null) {
 				if (s.length() > 0)
 					s.append(",");
-				s.append('[');
-				s.append(bucket.getStart());
-				s.append(',');
-				s.append(value);
-				s.append(']');
+				add(s, bucket, value);
 			}
 		}
 		s.insert(0, "{ \"data\": [");
-		s.append("] }");
-
+		s.append("]");
+		StringBuilder s2 = new StringBuilder();
+		Bucket b = buckets.getBucketForAll();
+		for (Metric m : Metric.values()) {
+			try {
+				Double value = b.get(m);
+				if (s2.length() > 0)
+					s2.append(",\n\t");
+				s2.append("\t\""
+						+ CaseFormat.LOWER_UNDERSCORE.to(
+								CaseFormat.LOWER_CAMEL, m.toString()) + "\": "
+						+ value);
+			} catch (RuntimeException e) {
+				// ignore unimplemented metric
+			}
+		}
+		s.append(",\n");
+		s.append("\"stats\": {\n");
+		s.append(s2);
+		s.append("    }\n");
+		s.append("}");
 		return s.toString();
+	}
+
+	private static void add(StringBuilder s, Bucket bucket, Double value) {
+		s.append('[');
+		s.append(bucket.getStart());
+		s.append(',');
+		s.append(value);
+		s.append(']');
 	}
 }
