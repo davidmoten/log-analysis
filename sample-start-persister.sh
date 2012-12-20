16624b720ab83532f1c3421deea3aabd97a1fe9e
Dave Moten <davidmoten@gmail.com>
2012-12-21 10:48:41 +1100

    moved sample config to test resources

--- sample-start-persister.sh
+++ sample-start-persister.sh
@@ -1,5 +1,5 @@
-#!/usr/bin/bash
+#!/bin/bash
 #Note the wildcard on the cts.log which will pick up the rolled over log4j logs
 cd `dirname $0`
-cd log-persister/ && mvn clean compile exec:java -Dexec.mainClass=org.moten.david.log.persister.Main -DlogPaths="/ausdev/container/logs/cts/cts.log.*,/ausdev/container/logs/cts/cts-adapter-flight-info-service.log" -Durl="remote:jenkins.amsa.gov.au/logs"
+cd log-persister/ && mvn clean compile exec:java -Dlogan.config=src/test/resources/sample-log-analysis-configuration.xml -Dlogan.db.url="remote:jenkins.amsa.gov.au/logs"
 
