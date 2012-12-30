475007e50eac602a13dc1f4067bd6c6a4c1e1cf0
Dave Moten <davidmoten@gmail.com>
2012-12-30 17:31:43 +1100

    write json to a stream (PrintWriter)

--- log-analysis-core/src/main/java/org/moten/david/log/query/Util.java
+++ log-analysis-core/src/main/java/org/moten/david/log/query/Util.java
@@ -1,5 +1,7 @@
 package org.moten.david.log.query;
 
+import java.io.PrintWriter;
+
 
 public class Util {
 
@@ -35,11 +37,51 @@ public class Util {
 		return s.toString();
 	}
 
+	public static void writeJson(Buckets buckets, Metric metric, PrintWriter writer) {
+		writer.print("{ \"data\": [");
+		boolean start = true;
+		for (Bucket bucket : buckets.getBuckets()) {
+			Double value = bucket.get(metric);
+			if (value != null) {
+				if (!start) 
+					writer.print(",");
+				else start = false;
+				add(writer, bucket, value);
+			}
+		}
+		writer.print("]");
+		StringBuilder s2 = new StringBuilder();
+		Bucket b = buckets.getBucketForAll();
+		for (Metric m : Metric.values()) {
+			try {
+				Double value = b.get(m);
+				if (s2.length() > 0)
+					s2.append(",\n\t");
+				s2.append("\t\"" + m + "\": " + value);
+			} catch (RuntimeException e) {
+				// ignore unimplemented metric
+			}
+		}
+		writer.println(",");
+		writer.println("\"stats\": {");
+		writer.print(s2);
+		writer.println("    }");
+		writer.print("}");
+	}
+	
 	private static void add(StringBuilder s, Bucket bucket, Double value) {
 		s.append('[');
 		s.append(bucket.getStart());
 		s.append(',');
 		s.append(value);
 		s.append(']');
+	}
+	
+	private static void add(PrintWriter w, Bucket bucket, Double value) {
+		w.print('[');
+		w.print(bucket.getStart());
+		w.print(',');
+		w.print(value);
+		w.print(']');
 	}
 }
