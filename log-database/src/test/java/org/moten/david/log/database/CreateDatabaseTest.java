170e053a3d7d8c5d4540960faa9afb9d3bd68cf2
Dave Moten <davidmoten@gmail.com>
2013-01-07 14:36:53 +1100

    create logs database before starting remote server

---
+++ log-database/src/test/java/org/moten/david/log/database/CreateDatabaseTest.java
@@ -0,0 +1,32 @@
+package org.moten.david.log.database;
+
+import static org.junit.Assert.assertNotNull;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.Test;
+import org.moten.david.log.core.Database;
+
+public class CreateDatabaseTest {
+
+	@Test
+	public void testCreateSoRemoteDatabaseWillLoadProperly() {
+
+		File orient = null;
+		for (File file : new File("target").listFiles()) {
+			if (file.getName().startsWith("orientdb-"))
+				orient = file;
+		}
+		assertNotNull("could not find orient home in target", orient);
+		File dbDirectory = new File(orient, "databases/logs");
+		try {
+			FileUtils.deleteDirectory(dbDirectory);
+		} catch (IOException e) {
+			// do nothing
+		}
+		Database db = new Database(dbDirectory);
+		db.close();
+	}
+}
