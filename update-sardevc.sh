8e7f6f325299eff01b780c17dcf46654890672c7
Dave Moten <davidmoten@gmail.com>
2013-02-05 15:36:28 +1100

    remove update sardevc script

--- update-sardevc.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd `dirname $0`
-mvn clean
-ssh aussar@sardevc.amsa.gov.au "rm -rf /ausdev/log-analysis/*"
-scp -pr log-server/ log-persister/ log-database/ log-ui/ log-analysis-core/ pom.xml *.sh aussar@sardevc:/ausdev/log-analysis/
